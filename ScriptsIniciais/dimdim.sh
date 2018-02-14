ba3bbf83eff270b4d446bde0287abb2c2bcdcbc6
gabriel vieira <gabrielvieira840@gmail.com>
2018-02-13 22:09:48 -0200

    Acrescentando Scripts basicos

---
+++ ScriptsIniciais/dimdim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "O custo deste curso eh +/- $1000"
+echo ""
+echo "O custo deste curso eh +/- \$1000"
---
+++ ScriptsIniciais/usandovar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+dia=02
+mes=03
+ano=1996
+
+echo "Eu nasci em $dia/$mes/$ano"
+echo "\-/" 
+var1=10
+var2=$var1
+
+echo "Variavel 1, vale = $var1"
+echo "Variavel 2, eh igual variavel 1, portanto eh igual a $var2" 
